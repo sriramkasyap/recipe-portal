10780114c1720dec1dd58c32240aaedc74ae040c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2024-12-07 13:11:54 +0530

    fix: text

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -5,18 +5,21 @@ const LoginScreen = () => {
   const { handleAuthSuccess, handleAuthError } = useAppContext();
 
   return (
-    <div className="flex w-full h-screen justify-center items-center flex-col gap-20">
-      <h1 className="text-4xl font-bold">Welcome to VivaRecipes</h1>
+    <div className="flex w-full h-screen justify-center items-center flex-col gap-5">
+      <img src="/VivaRecipe.png" alt="Viva Recipes" className="w-48 " />
+      <h1 className="text-4xl font-bold">Welcome to VivaRecipe</h1>
       <div className="flex flex-col gap-5 items-center">
-        <h2 className="text-2xl font-bold text-center">
-          Sign in to your account to continue
-        </h2>
-        <GoogleLogin
-          onSuccess={handleAuthSuccess}
-          onError={handleAuthError}
-          size="large"
-          theme="filled_black"
-        />
+        <p className="text-2xl font-medium text-center">
+          Sign in to your account to get started
+        </p>
+        <div className="w-full my-10">
+          <GoogleLogin
+            onSuccess={handleAuthSuccess}
+            onError={handleAuthError}
+            size="large"
+            theme="filled_black"
+          />
+        </div>
       </div>
     </div>
   );
